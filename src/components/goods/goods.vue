3e58a7de91a38e2d7f5b1e7512f32cad1787ccb3
Zadd7 <coder_zadd7@163.com>
2017-05-13 22:18:52 +0800

    开始编写goods组件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,13 +1,47 @@
 <template>
-    <div class="goods-content">
-    	
+    <div class="goods">
+    	<div class="menu-wrapper"></div>
+    	<div class="foods-wrapper"></div>
     </div>
 </template>
 
-<script type="ecmascript-6">
-    
+<script>
+const ERR_OK = 0;
+
+export default {
+    data() {
+            return {
+                goods: []
+            }
+        },
+        props: {
+            seller: {
+                type: Object
+            }
+        },
+        creaded() {
+            this.axios.get("/api/goods").then((res) => {
+                if (res.data.errno == ERR_OK) {
+                    this.goods = res.data.data;
+                }
+            })
+        }
+}
 </script>
 
-<style rel="stylesheet">
-    
+
+<style lang="stylus" rel="stylesheet/stylus">
+    .goods
+    	display:flex
+	    position:absolute
+	    top:174px
+	    bottom:46px
+	    width:100%
+	    overflow:hidden
+	    .menu-wrapper
+	    	flex:0 0 80px
+	    	width:80px
+	    	background-color:#f3f5f7
+	    .foods-wrapper
+	    	flex:1
 </style>
