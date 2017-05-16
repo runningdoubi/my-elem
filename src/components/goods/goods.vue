7ecdf149311deafaa2f7e0141a011a604b7b08a4
Zadd7 <coder_zadd7@163.com>
2017-05-16 23:11:06 +0800

    stylus问题不明，先换回原生css，good组件滚动完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,51 +1,51 @@
 <template>
-   <div class="goods">
-    <div class="menu-wrapper">
-        <ul>
-            <li v-for="item in goods" class="menu-item">
-                <span class="text"><span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}</span>
-            </li>
-        </ul>
+    <div class="goods">
+        <div class="menu-wrapper" ref="menuWrapper">
+            <ul>
+                <li v-for="item in goods" class="menu-item">
+                    <span class="text">
+                	<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span> {{item.name}}
+                    </span>
+                </li>
+            </ul>
+        </div>
+        <div class="foods-wrapper" ref="foodsWrapper">
+            <ul>
+                <li v-for="item in goods" class="food-list">
+                    <h1 class="title">{{item.name}}</h1>
+                    <ul>
+                        <li v-for="food in item.foods" class="food-item">
+                            <div class="icon">
+                                <img :src="food.icon" width="57" height="57" alt="">
+                            </div>
+                            <div class="content">
+                                <h2 class="name">{{food.name}}</h2>
+                                <p class="desc">{{food.description}}</p>
+                                <div class="extra">
+                                    <span class="count">月售{{food.sellCount}}份</span>
+                                    <span>好评率{{food.rating}}%</span>
+                                </div>
+                                <div class="price">
+                                    <span class="now">¥{{food.price}}</span>
+                                    <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
-    <div class="foods-wrapper">
-    	<ul>
-    		<li v-for="item in goods" class="food-list">
-    			<h1 class="title">{{item.name}}</h1>
-    			<ul>
-    				<li v-for="food in item.foods" class="food-item">
-    					<div class="icon">
-    						<img :src="food.icon" width="57" height="57" alt="">
-    					</div>
-    					<div class="content">
-    						<h2 class="name">{{food.name}}</h2>
-    						<p class="desc">{{food.description}}</p>
-    						<div class="extra">
-    							<span>月售{{food.sellCount}}份</span>
-    							<span>好评率{{food.rating}}%</span>
-    						</div>
-    						<div class="price">
-    							<span>￥{{food.price}}</span>
-    							<span v-show="food.oldPrice">{{food.oldPrice}}</span>
-    						</div>
-    					</div>
-    				</li>
-    			</ul>
-    		</li>
-    	</ul>
-    </div>
-</div>
-
 </template>
-
-
 <script>
+import BScroll from "better-scroll";
 const ERR_OK = 0;
 
 export default {
     data() {
             return {
                 goods: [],
-                classMap:[]
+                classMap: []
             }
         },
         props: {
@@ -57,87 +57,161 @@ export default {
             this.axios.get("/api/goods").then((res) => {
                 if (res.data.erron == ERR_OK) {
                     this.goods = res.data.data;
+                    this.$nextTick(function() {
+                    	this._initScroll();
+                    })
                 }
             });
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+        },
+        methods:{
+        	_initScroll(){
+        		this.menuScroll = new BScroll(this.$refs.menuWrapper,{});
+
+        		this.foosScroll = new BScroll(this.$refs.foodsWrapper,{})
+        	}
         }
 }
 </script>
+<style type="text/css" scoped>
+.goods {
+    display: flex;
+    position: absolute;
+    top: 175px;
+    bottom: 46px;
+    width: 100%;
+    overflow: hidden;
+    font-size: 0;
+}
 
+.goods .menu-wrapper {
+    flex: 0 0 80px;
+    width: 80px;
+    background-color: #f3f5f7;
+}
 
-<style lang="stylus" rel="stylesheet/stylus" scoped>
-    .goods
-    	display:flex
-	    position:absolute
-	    top:175px
-	    bottom:46px
-	    width:100%
-	    overflow:hidden
-	    .menu-wrapper
-	    	flex:0 0 80px
-	    	width:80px
-	    	background-color:#f3f5f7
-	    	.menu-item
-	    		display:table
-	    		height:54px
-	    		width:56px
-	    		line-height:14px
-	    		font-size:14px
-	    		padding:0 12px
-	    		border-bottom: 1px solid rgba(7, 17, 17, .1)
-	    		.icon
-	    			display: inline-block
-				    vertical-align: top
-				    width: 12px
-				    height: 12px
-				    margin-right: 2px
-				    background-size: 12px 12px
-				    background-repeat: no-repeat
-				    &.decrease
-				    	background-image: url('./img/decrease_3@2x.png')
-				    &.discount
-				    	background-image: url('./img/discount_3@2x.png')
-				    &.guarantee
-				    	background-image: url('./img/guarantee_3@2x.png')
-				    &.invoice
-				    	background-image: url('./img/invoice_3@2x.png')
-				    &.special
-				    	background-image: url('./img/special_3@2x.png')
-				.text
-					display:table-cell
-					width:56px
-					vertical-align:middle
-					font-size:12px
-	    .foods-wrapper
-	    	flex:1
-	    	.title
-	    		padding-left:14px
-	    		height:26px
-	    		line-height:26px
-	    		border:2px solid #d9dd1
-	    		font-size:12px
-	    		color:rgb(147,153,159)
-	    		background-color:#f3f5f7
-	    	.food-item
-	    		display:flex
-	    		margin:18px
-	    		padding-bottom:18px
-	    		border-top:1px solid rgba(255, 255, 255, .2)
-	    		&:last-child
-	    			border-top:none 
-	    			margin-bottom:0
-	    		.icon
-	    			flex:0 0 57px
-	    			width:57px
-	    			height:57px
-	    			margin-right:10px
-	    		.content
-	    			flex:1
-	    			.name
-	    				margin:2px 0 8px 0
-	    				height:14px
-	    				line-height:14px
-	    				font-size:14px
-	    				color:rgb(7,17,27)
-	    				
+.goods .menu-wrapper .menu-item {
+    display: table;
+    height: 54px;
+    width: 56px;
+    line-height: 14px;
+    font-size: 14px;
+    padding: 0 12px;
+    border-bottom: 1px solid rgba(7, 17, 17, .1);
+}
+
+.goods .menu-wrapper .menu-item .icon {
+    display: inline-block;
+    vertical-align: top;
+    width: 12px;
+    height: 12px;
+    margin-right: 2px;
+    background-size: 12px 12px;
+    background-repeat: no-repeat;
+}
+
+.goods .menu-wrapper .menu-item .icon.decrease {
+    background-image: url('./img/decrease_3@2x.png');
+}
+
+.goods .menu-wrapper .menu-item .icon.discount {
+    background-image: url('./img/discount_3@2x.png');
+}
+
+.goods .menu-wrapper .menu-item .icon.guarantee {
+    background-image: url('./img/guarantee_3@2x.png');
+}
+
+.goods .menu-wrapper .menu-item .icon.invoice {
+    background-image: url('./img/invoice_3@2x.png');
+}
+
+.goods .menu-wrapper .menu-item .icon.special {
+    background-image: url('./img/special_3@2x.png')
+}
+
+.goods .menu-wrapper .menu-item .text {
+    display: table-cell;
+    width: 56px;
+    vertical-align: middle;
+    font-size: 12px;
+}
+
+.goods .foods-wrapper {
+    flex: 1;
+}
+
+.goods .foods-wrapper .title {
+    padding-left: 14px;
+    height: 26px;
+    line-height: 26px;
+    border: 2px solid #d9dd1;
+    font-size: 12px;
+    color: rgb(147, 153, 159);
+    background-color: #f3f5f7;
+}
+
+.goods .foods-wrapper .food-item {
+    display: flex;
+    margin: 18px;
+    padding-bottom: 18px;
+    border-top: 1px solid rgba(255, 255, 255, .2);
+}
+
+.goods .foods-wrapper .food-item:last-child {
+    border-top: none;
+    margin-bottom: 0;
+}
+
+.goods .foods-wrapper .food-item .icon {
+    flex: 0 0 57px;
+    width: 57px;
+    height: 57px;
+    margin-right: 10px;
+}
+
+.goods .foods-wrapper .food-item .content {
+    flex: 1;
+}
+
+.goods .foods-wrapper .food-item .content .name {
+    margin: 2px 0 8px 0;
+    height: 14px;
+    line-height: 14px;
+    font-size: 14px;
+    color: rgb(7, 17, 27);
+}
+
+.goods .foods-wrapper .food-item .content .desc {
+    margin-bottom: 8px;
+    line-height: 12px;
+    font-size: 10px;
+    color: rgb(147, 153, 159);
+}
+
+.goods .foods-wrapper .food-item .content .extra {
+    line-height: 10px;
+    line-height: 10px;
+    font-size: 10px;
+    color: rgb(147, 153, 159);
+}
+
+.goods .foods-wrapper .food-item .content .extra .count {
+    margin-right: 12px;
+}
+.goods .foods-wrapper .food-item .content .price{
+	font-weight: 700;
+	line-height: 24px;
+
+}
+.goods .foods-wrapper .food-item .content .price .now{
+	margin-right: 8px;
+	font-size: 14px;
+	color: rgb(240,20,20);
+}
+.goods .foods-wrapper .food-item .content .price .old{
+	text-decoration: line-through;
+	font-size: 10px;
+	color: rgb(147, 153, 159);
+}
 </style>
