d53ad6329adb7eac31cd2147d988140b1a9b2499
Zadd7 <coder_zadd7@163.com>
2017-07-09 17:58:40 +0800

    'ratingselect组件完成'

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="cartcontrol">
         <transition name="move">
-            <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart($event)">
+            <div class="cart-decrease" v-show="food.count>0" @click.stop.prevent="decreaseCart($event)">
                 <span class="icon-remove_circle_outline inner"></span>
             </div>
         </transition>
         <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
-        <div class="cart-add icon-add_circle" @click="addCart($event)"></div>
+        <div class="cart-add icon-add_circle" @click.stop.prevnet="addCart($event)"></div>
     </div>
 </template>
 <script type="text/javascript">
--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -19,12 +19,22 @@
                             <span class="now">¥{{food.price}}</span>
                             <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                         </div>
+                        <div class="cartcontrol-wrapper">
+                            <cartcontrol :food="food"></cartcontrol>
+                        </div>
+                        <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count==0">
+                            加入购物车
+                        </div>
                     </div>
-                    <div class="cartcontrol-wrapper">
-                    	<cartcontrol :food="food"></cartcontrol>
+                    <split v-show="food.info"></split>
+                    <div class="info" v-show="food.info">
+                        <h1 class="title">商品信息</h1>
+                        <p class="text">{{food.info}}</p>
                     </div>
-                    <div @click="addFirst" class="buy"v-show="!food.count || food.count==0">
-                    	加入购物车
+                    <split></split>
+                    <div class="rating">
+                        <h1 class="title">商品评价</h1>
+                        <ratingselect :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
                     </div>
                 </div>
             </div>
@@ -34,6 +44,12 @@
 <script type="text/javascript">
 import BScroll from "better-scroll";
 import cartcontrol from "../cartcontrol/cartcontrol";
+import split from "../split/split";
+import ratingselect from "../ratingselect/ratingselect";
+
+const POSITIVE = 0;
+const NEGATIVE = 1;
+const ALL = 2;
 
 export default {
     props: {
@@ -43,33 +59,44 @@ export default {
     },
     data() {
         return {
-            showFlag: false
+            showFlag: false,
+            selectType: ALL,
+            onlyContent: true,
+            desc: {
+                all: '全部',
+                positive: '推荐',
+                negative: '吐槽'
+            }
         }
     },
-    components:{
-    	cartcontrol
+    components: {
+        cartcontrol,
+        split,
+        ratingselect
     },
     methods: {
         show() {
             this.showFlag = true;
+            this.selectType = ALL;
+            this.onlyContent = true;
             this.$nextTick(() => {
                 if (!this.scroll) {
                     this.scroll = new BScroll(this.$refs.food, {
                         click: true
                     });
                 } else {
-                	this.scroll.refresh();
+                    this.scroll.refresh();
                 }
             })
         },
         hide() {
             this.showFlag = false;
         },
-        addFirst(event){
-        	if(!event._constructed){
-        		return;
-        	}
-        	this.$set(this.food,"count",1);
+        addFirst(event) {
+            if (!event._constructed) {
+                return;
+            }
+            this.$set(this.food, "count", 1);
         }
     }
 }
@@ -124,6 +151,7 @@ export default {
 }
 
 .content {
+    position: relative;
     padding: 18px;
 }
 
@@ -168,23 +196,52 @@ export default {
     font-size: 10px;
     color: rgb(147, 153, 159);
 }
-.cartcontrol-wrapper{
-	position: absolute;
-	right: 12px;
-	bottom: 12px;
+
+.cartcontrol-wrapper {
+    position: absolute;
+    right: 12px;
+    bottom: 12px;
 }
-.buy{
-	position: absolute;
-	right: 18px;
-	bottom: 18px;
-	z-index: 10;
-	height: 24px;
-	line-height: 24px;
-	padding: 0 12px;
-	box-sizing: border-box;
-	border-radius: 12px;
-	font-size: 10px;
-	color: #fff;
-	background-color: rgb(0,160,220);
+
+.buy {
+    position: absolute;
+    right: 18px;
+    bottom: 18px;
+    z-index: 10;
+    height: 24px;
+    line-height: 24px;
+    padding: 0 12px;
+    box-sizing: border-box;
+    border-radius: 12px;
+    font-size: 10px;
+    color: #fff;
+    background-color: rgb(0, 160, 220);
+}
+
+.info{
+    padding: 18px;
+}
+
+.info .title {
+    line-height: 14px;
+    margin-bottom: 6px;
+    font-size: 14px;
+    color: rgb(7, 17, 27);
+}
+
+.info .text {
+    line-height: 24px;
+    padding: 0 8px;
+    font-size: 12px;
+    color: rgb(77, 85, 93);
+}
+.rating{
+    padding-top: 18px;
+}
+.rating .title {
+    line-height: 14px;
+    margin-left: 18px;
+    font-size: 14px;
+    color: rgb(7, 17, 27);
 }
 </style>
